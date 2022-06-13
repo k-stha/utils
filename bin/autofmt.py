c07f30650cbadb160c4ef0eb78cb48a5a73d1771
Kushal Shrestha <81743741+k-stha@users.noreply.github.com>
2022-06-13 01:47:51 -0400

    Improve variable names and fixed errors
    The following changes are made:
    
    - The missing comma is added
    - The name file(s) are renamed to path(s)
    - A new function is created to check path(s), and run isort and black
    
    Signed-off-by: Kushal Shrestha <81743741+k-stha@users.noreply.github.com>

--- bin/autofmt.py
+++ bin/autofmt.py
@@ -10,10 +10,7 @@ from subprocess import run
 
 def error(message: str) -> None:
     """Print error message and send it to stderr."""
-    try:
-        print(message, file=sys.stderr)
-    except IOError:
-        pass
+    print(message, file=sys.stderr)
 
 
 def get_args(argv: list[str]) -> argparse.Namespace:
@@ -21,11 +18,11 @@ def get_args(argv: list[str]) -> argparse.Namespace:
     parser: argparse.ArgumentParser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "input_file",
+        "input_path",
         type=str,
         action="extend",
-        nargs="+"
-        help="Input file(s) to format",
+        nargs="+",
+        help="Input path(s) to format",
     )
 
     return parser.parse_intermixed_args(argv)
@@ -44,7 +41,7 @@ def check_path(path_list: list[str]) -> None:
         sys.exit(1)
 
 
-def return_isort_args(files: list[str]) -> list[str]:
+def return_isort_args(paths: list[str]) -> list[str]:
     """Return isort arguments as list."""
     isort_exec = "isort"
 
@@ -62,31 +59,36 @@ def return_isort_args(files: list[str]) -> list[str]:
         "0",
         "--up",
         "--remove-redundant-aliases",
-    ] + files
+    ] + paths
 
     return isort_args
 
 
-def return_black_args(files: list[str]) -> list[str]:
+def return_black_args(paths: list[str]) -> list[str]:
     """Return black arguments as list."""
     black_exec = "black"
 
-    black_args = [black_exec, "-q"] + files
+    black_args = [black_exec, "-q"] + paths
 
     return black_args
+
+
+def run_formatters(paths: list[str]) -> None:
+    """"""
+    check_path(paths)
+
+    run(return_isort_args(paths), check=True)
+    run(return_black_args(paths), check=True)
 
 
 def main(argv: list[str]) -> None:
     """Function to be called."""
     args: argparse.Namespace = get_args(argv)
 
-    if args.input_file:
-        files = args.input_file
+    if args.input_path:
+        paths: list[str] = args.input_path
 
-    check_path(files)
-
-    run(return_isort_args(files), check=True)
-    run(return_black_args(files), check=True)
+    run_formatters(paths)
 
 
 if __name__ == "__main__":
